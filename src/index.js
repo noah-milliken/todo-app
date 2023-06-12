0d96fb72cf5ed5733390ffae65262ba91ac22f56
noah milliken <noahmilliken@gmail.com>
2023-06-12 15:14:15 -0400

    factory function and index liked and functioning

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import model from './model.js';
-import { form } from './view.js';
+import { form } from "./view"
+import { todoModel } from "./model"
 
-const component = () => {
+function component() {
+
+    let model = todoModel()
+    model.deleteTodo(10)
+    console.log(model.sortBy(model.todos, 'category'))
+
     const element = document.createElement('div')
-
-
     element.appendChild(form())
+    document.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const category = document.getElementById('categoryInput').value
+        const title = document.getElementById('titleInput').value
+        const description = document.getElementById('descriptionInput').value
+        const dueDate = document.getElementById('dueDateInput').value
+        const priorityInput = document.getElementById('priorityInput').value
+        const completed = document.getElementById('completedInput').value
+
+        model.addTodo(category, title, description, dueDate, priorityInput, completed)
+        console.log(model.sortBy(model.todos, 'category'))
+    })
     return element
 }
 
 document.body.appendChild(component())
+
--- src/view.js
+++ src/view.js
@@ -1,20 +1,19 @@
 
-const domModel = () => {
-    const form = () => {
-        const formElement = document.createElement('div')
-        formElement.innerHTML = `
+const form = () => {
+  const formElement = document.createElement('div')
+  formElement.innerHTML = `
     <form id="todoForm">
   <label for="category">Category:</label>
-  <input type="text" id="categoryInput" name="category" required>
+  <input type="text" id="categoryInput" name="category" value="Coding" required>
   
   <label for="title">Title:</label>
-  <input type="text" id="titleInput" name="title" required>
+  <input type="text" id="titleInput" name="title" value="Coding" required>
   
   <label for="description">Description:</label>
-  <input type="text" id="descriptionInput" name="description" required>
+  <input type="text" id="descriptionInput" name="description" value="Practice factory functions" required>
   
   <label for="dueDate">Due Date:</label>
-  <input type="date" id="dueDateInput" name="dueDate" required>
+  <input type="date" id="dueDateInput" name="dueDate"  value="04-07-2023" required>
   
   <label for="priority">Priority:</label>
   <select id="priorityInput" name="priority" required>
@@ -29,8 +28,7 @@ const domModel = () => {
   <button type="submit">Add Todo</button>
 </form>
     `
-        return formElement
-    }
-    return form
+  return formElement
 }
-export default domModel
+
+export { form }
